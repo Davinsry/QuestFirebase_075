a5f09d1762c6e3bfaf5825457bd0979fd9092b91
Davinsry <davinipad53@gmail.com>
2026-01-02 16:25:01 +0700

    menabahkan fungsi dari DestinasiEdit.kt

--- app/src/main/java/com/davin/questfirebase_075/view/route/DestinasiEdit.kt
+++ app/src/main/java/com/davin/questfirebase_075/view/route/DestinasiEdit.kt
@@ -1,4 +1,11 @@
 package com.davin.questfirebase_075.view.route
 
-class DestinasiEdit {
+import com.davin.questfirebase_075.R
+import com.davin.questfirebase_075.view.route.DestinasiNavigasi
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
