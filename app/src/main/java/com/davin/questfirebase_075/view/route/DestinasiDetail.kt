5982669daa93a14f06596c3b3d4f264a356a5f52
Davinsry <davinipad53@gmail.com>
2026-01-02 14:36:42 +0700

    Menambah object DestinasiDetail.kt

--- app/src/main/java/com/davin/questfirebase_075/view/route/DestinasiDetail.kt
+++ app/src/main/java/com/davin/questfirebase_075/view/route/DestinasiDetail.kt
@@ -1,4 +1,11 @@
 package com.davin.questfirebase_075.view.route
 
-class DestinasiDetail {
+import com.davin.questfirebase_075.R
+import com.davin.questfirebase_075.view.route.DestinasiNavigasi
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
