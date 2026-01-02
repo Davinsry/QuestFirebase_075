91afe64f1e5e84a5cdd19bcec521af220f57a282
Davinsry <davinipad53@gmail.com>
2026-01-02 16:25:25 +0700

    menabahkan fungsi dari DestinasiHome.kt

--- app/src/main/java/com/davin/questfirebase_075/view/route/DestinasiHome.kt
+++ app/src/main/java/com/davin/questfirebase_075/view/route/DestinasiHome.kt
@@ -1,2 +1,9 @@
 package com.davin.questfirebase_075.view.route
 
+import com.davin.questfirebase_075.R
+import com.davin.questfirebase_075.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
