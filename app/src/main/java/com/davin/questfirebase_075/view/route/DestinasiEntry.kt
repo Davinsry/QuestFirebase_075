7dfcf04ef125756a6c7c100af620e029f0d7146d
Davinsry <davinipad53@gmail.com>
2026-01-02 16:25:13 +0700

    menabahkan fungsi dari DestinasiEntry.kt

--- app/src/main/java/com/davin/questfirebase_075/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/davin/questfirebase_075/view/route/DestinasiEntry.kt
@@ -1,2 +1,9 @@
 package com.davin.questfirebase_075.view.route
 
+import com.davin.questfirebase_075.R
+import com.davin.questfirebase_075.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
