d5345d15b0a1f30f19d5c3ce00fd89826864f693
Davinsry <davinipad53@gmail.com>
2026-01-02 16:28:42 +0700

    peruabhan fungsi pada MainActivity.kt

--- app/src/main/java/com/davin/questfirebase_075/MainActivity.kt
+++ app/src/main/java/com/davin/questfirebase_075/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.davin.questfirebase_075.navigation.DataSiswaApp
 import com.davin.questfirebase_075.ui.theme.QuestFirebase_075Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +16,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            // Sesuaikan nama tema dengan yang ada di folder ui.theme/Theme.kt
             QuestFirebase_075Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    // Pastikan composable DataSiswaApp sudah dibuat (biasanya berisi NavHost)
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestFirebase_075Theme {
-        Greeting("Android")
     }
 }
