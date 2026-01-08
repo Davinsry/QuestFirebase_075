dcba9a288da2470713a9ae6775f42fc20011ab19
Davinsry <davinipad53@gmail.com>
2026-01-08 13:04:54 +0700

    menambahkan fun EditSiswaScreen

--- app/src/main/java/com/davin/questfirebase_075/view/HalamanEdit.kt
+++ app/src/main/java/com/davin/questfirebase_075/view/HalamanEdit.kt
@@ -1,2 +1,25 @@
 package com.davin.questfirebase_075.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.davin.questfirebase_075.view.route.DestinasiEdit
+import com.davin.questfirebase_075.viewmodel.EditViewModel
+import com.davin.questfirebase_075.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
