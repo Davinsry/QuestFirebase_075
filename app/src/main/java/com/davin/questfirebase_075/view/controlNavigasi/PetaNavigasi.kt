f8a4ab260b30378dafe9325e62f12e6d3850d36b
Davinsry <davinipad53@gmail.com>
2026-01-08 13:11:23 +0700

    menambahkan composable destinasi edit pada PetaNavigasi.kt

--- app/src/main/java/com/davin/questfirebase_075/view/controlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/davin/questfirebase_075/view/controlNavigasi/PetaNavigasi.kt
@@ -61,7 +61,16 @@ fun HostNavigasi(
                 navigateBack = { navController.navigate(DestinasiHome.route) }
             )
         }
-
+        composable(
+            DestinasiEdit.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiEdit.itemIdArg) {
+                type = NavType.StringType
+            })
+        ) {
+            EditSiswaScreen(
+                navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() }
+            )
         }
     }
 }
