926d8a5f1bc7047afa56037ce52b63bf3c3cd601
Davinsry <davinipad53@gmail.com>
2026-01-08 13:08:01 +0700

    menambahkan sealed interfaces statusUIDetail

--- app/src/main/java/com/davin/questfirebase_075/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/davin/questfirebase_075/viewmodel/EditViewModel.kt
@@ -1,2 +1,61 @@
 package com.davin.questfirebase_075.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.davin.questfirebase_075.modeldata.DetailSiswa
+import com.davin.questfirebase_075.modeldata.UIStateSiswa
+import com.davin.questfirebase_075.modeldata.toDataSiswa
+import com.davin.questfirebase_075.modeldata.toUiStateSiswa
+import com.davin.questfirebase_075.repositori.RepositorySiswa
+import com.davin.questfirebase_075.view.route.DestinasiDetail
+import com.davin.questfirebase_075.view.route.DestinasiEdit
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    // Mengambil ID sebagai String -> Long
+    private val idSiswa: Long = savedStateHandle.get<String>(DestinasiEdit.itemIdArg)?.toLong()
+        ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            // Tanda !! berarti kita yakin data ada (force unwrap) sesuai gambar
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            try {
+                repositorySiswa.editSatuSiswa(idSiswa, uiStateSiswa.detailSiswa.toDataSiswa())
+                println("Update Sukses: $idSiswa")
+            } catch (e: Exception) {
+                println("Update Error: ${e.message}")
+            }
+        }
+    }
+}
