33f02b6442ac51c9498411900b012d11e26c3cba
Davinsry <davinipad53@gmail.com>
2026-01-08 11:37:20 +0700

    menambahkan seaked interface

--- app/src/main/java/com/davin/questfirebase_075/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/davin/questfirebase_075/viewmodel/DetailViewModel.kt
@@ -1,4 +1,21 @@
 package com.davin.questfirebase_075.viewmodel
 
-class DetailViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.davin.questfirebase_075.modeldata.Siswa
+import com.davin.questfirebase_075.repositori.RepositorySiswa
+import com.davin.questfirebase_075.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+// Status UI untuk halaman Detail
+sealed interface StatusUIDetail {
+    data class Success(val satuSiswa: Siswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
 }
+
