42c1d283799f5e15b45112c39d71e2aba44caf6b
Davinsry <davinipad53@gmail.com>
2026-01-08 13:07:35 +0700

    menambahkan class DetailViewModel

--- app/src/main/java/com/davin/questfirebase_075/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/davin/questfirebase_075/viewmodel/DetailViewModel.kt
@@ -12,48 +12,6 @@ import com.davin.questfirebase_075.view.route.DestinasiDetail
 import kotlinx.coroutines.launch
 import java.io.IOException
 
-// Status UI untuk halaman Detail
-sealed interface StatusUIDetail {
-    data class Success(val satuSiswa: Siswa) : StatusUIDetail
-    object Error : StatusUIDetail
-    object Loading : StatusUIDetail
-}
+
 
 class DetailViewModel(
-    savedStateHandle: SavedStateHandle,
-    private val repositorySiswa: RepositorySiswa
-) : ViewModel() {
-
-    // Mengambil idSiswa dari argumen navigasi (SavedStateHandle)
-    private val idSiswa: Long = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
-
-    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
-        private set
-
-    init {
-        getSatuSiswa()
-    }
-
-    fun getSatuSiswa() {
-        viewModelScope.launch {
-            statusUIDetail = StatusUIDetail.Loading
-            statusUIDetail = try {
-                StatusUIDetail.Success(repositorySiswa.getSiswaById(idSiswa))
-            } catch (e: IOException) {
-                StatusUIDetail.Error
-            } catch (e: Exception) {
-                StatusUIDetail.Error
-            }
-        }
-    }
-
-    // Fungsi suspend agar bisa dipanggil di Coroutine scope milik UI (Composable)
-    suspend fun hapusSatuSiswa() {
-        try {
-            repositorySiswa.deleteSiswa(idSiswa)
-        } catch (e: Exception) {
-            // Handle error jika diperlukan
-            println("Gagal Hapus Data: ${e.message}")
-        }
-    }
-}
